16f94dbf4b59a0e64bcc5974ac39e086937f362c
deepankargarg5-maker <deepankar.garg5@gmail.com>
2025-08-26 19:29:04 +0530

    Update Set5b.hs

--- exercises/Set5b.hs
+++ exercises/Set5b.hs
@@ -1,206 +1,78 @@
--- Exercise set 5b: playing with binary trees
-
 module Set5b where
 
 import Mooc.Todo
 
--- The next exercises use the binary tree type defined like this:
-
 data Tree a = Empty | Node a (Tree a) (Tree a)
   deriving (Show, Eq)
 
-------------------------------------------------------------------------------
--- Ex 1: implement the function valAtRoot which returns the value at
--- the root (top-most node) of the tree. The return value is Maybe a
--- because the tree might be empty (i.e. just a Empty)
-
+-- Ex 1
 valAtRoot :: Tree a -> Maybe a
-valAtRoot t = todo
+valAtRoot Empty        = Nothing
+valAtRoot (Node v _ _) = Just v
 
-------------------------------------------------------------------------------
--- Ex 2: compute the size of a tree, that is, the number of Node
--- constructors in it
---
--- Examples:
---   treeSize (Node 3 (Node 7 Empty Empty) Empty)  ==>  2
---   treeSize (Node 3 (Node 7 Empty Empty) (Node 1 Empty Empty))  ==>  3
-
+-- Ex 2
 treeSize :: Tree a -> Int
-treeSize t = todo
+treeSize Empty = 0
+treeSize (Node _ left right) = 1 + treeSize left + treeSize right
 
-------------------------------------------------------------------------------
--- Ex 3: get the largest value in a tree of positive Ints. The
--- largest value of an empty tree should be 0.
---
--- Examples:
---   treeMax Empty  ==>  0
---   treeMax (Node 3 (Node 5 Empty Empty) (Node 4 Empty Empty))  ==>  5
-
+-- Ex 3
 treeMax :: Tree Int -> Int
-treeMax = todo
+treeMax Empty = 0
+treeMax (Node v left right) = maximum [v, treeMax left, treeMax right]
 
-------------------------------------------------------------------------------
--- Ex 4: implement a function that checks if all tree values satisfy a
--- condition.
---
--- Examples:
---   allValues (>0) Empty  ==>  True
---   allValues (>0) (Node 1 Empty (Node 2 Empty Empty))  ==>  True
---   allValues (>0) (Node 1 Empty (Node 0 Empty Empty))  ==>  False
-
+-- Ex 4
 allValues :: (a -> Bool) -> Tree a -> Bool
-allValues condition tree = todo
+allValues _ Empty = True
+allValues cond (Node v left right) = cond v && allValues cond left && allValues cond right
 
-------------------------------------------------------------------------------
--- Ex 5: implement map for trees.
---
--- Examples:
---
--- mapTree (+1) Empty  ==>  Empty
--- mapTree (+2) (Node 0 (Node 1 Empty Empty) (Node 2 Empty Empty))
---   ==> (Node 2 (Node 3 Empty Empty) (Node 4 Empty Empty))
-
+-- Ex 5
 mapTree :: (a -> b) -> Tree a -> Tree b
-mapTree f t = todo
+mapTree _ Empty = Empty
+mapTree f (Node v left right) = Node (f v) (mapTree f left) (mapTree f right)
 
-------------------------------------------------------------------------------
--- Ex 6: given a value and a tree, build a new tree that is the same,
--- except all nodes that contain the value have been removed. Also
--- remove the subnodes of the removed nodes.
---
--- Examples:
---
---     1          1
---    / \   ==>    \
---   2   0          0
---
---  cull 2 (Node 1 (Node 2 Empty Empty)
---                 (Node 0 Empty Empty))
---     ==> (Node 1 Empty
---                 (Node 0 Empty Empty))
---
---      1           1
---     / \           \
---    2   0   ==>     0
---   / \
---  3   4
---
---  cull 2 (Node 1 (Node 2 (Node 3 Empty Empty)
---                         (Node 4 Empty Empty))
---                 (Node 0 Empty Empty))
---     ==> (Node 1 Empty
---                 (Node 0 Empty Empty)
---
---    1              1
---   / \              \
---  0   3    ==>       3
---   \   \
---    2   0
---
---  cull 0 (Node 1 (Node 0 Empty
---                         (Node 2 Empty Empty))
---                 (Node 3 Empty
---                         (Node 0 Empty Empty)))
---     ==> (Node 1 Empty
---                 (Node 3 Empty Empty))
-
+-- Ex 6
 cull :: Eq a => a -> Tree a -> Tree a
-cull val tree = todo
+cull _ Empty = Empty
+cull val (Node v left right)
+  | v == val  = Empty
+  | otherwise = Node v (cull val left) (cull val right)
 
-------------------------------------------------------------------------------
--- Ex 7: check if a tree is ordered. A tree is ordered if:
---  * all values to the left of the root are smaller than the root value
---  * all of the values to the right of the root are larger than the root value
---  * and the left and right subtrees are ordered.
---
--- Hint: allValues will help you here!
---
--- Examples:
---         1
---        / \   is ordered:
---       0   2
---   isOrdered (Node 1 (Node 0 Empty Empty)
---                     (Node 2 Empty Empty))   ==>   True
---
---         1
---        / \   is not ordered:
---       2   3
---   isOrdered (Node 1 (Node 2 Empty Empty)
---                     (Node 3 Empty Empty))   ==>   False
---
---           2
---         /   \
---        1     3   is not ordered:
---         \
---          0
---   isOrdered (Node 2 (Node 1 Empty
---                             (Node 0 Empty Empty))
---                     (Node 3 Empty Empty))   ==>   False
---
---           2
---         /   \
---        0     3   is ordered:
---         \
---          1
---   isOrdered (Node 2 (Node 0 Empty
---                             (Node 1 Empty Empty))
---                     (Node 3 Empty Empty))   ==>   True
-
+-- Ex 7
 isOrdered :: Ord a => Tree a -> Bool
-isOrdered = todo
-
-------------------------------------------------------------------------------
--- Ex 8: a path in a tree can be represented as a list of steps that
--- go either left or right.
+isOrdered Empty = True
+isOrdered (Node v left right) =
+  allValues (< v) left &&
+  allValues (> v) right &&
+  isOrdered left &&
+  isOrdered right
 
 data Step = StepL | StepR
   deriving (Show, Eq)
 
--- Define a function walk that takes a tree and a list of steps, and
--- returns the value at that point. Return Nothing if you fall of the
--- tree (i.e. hit a Empty).
---
--- Examples:
---   walk [] (Node 1 (Node 2 Empty Empty) Empty)       ==>  Just 1
---   walk [StepL] (Node 1 (Node 2 Empty Empty) Empty)  ==>  Just 2
---   walk [StepL,StepL] (Node 1 (Node 2 Empty Empty) Empty)  ==>  Nothing
-
+-- Ex 8
 walk :: [Step] -> Tree a -> Maybe a
-walk = todo
+walk [] Empty = Nothing
+walk [] (Node v _ _) = Just v
+walk (StepL:steps) (Node _ left _) = walk steps left
+walk (StepR:steps) (Node _ _ right) = walk steps right
+walk _ Empty = Nothing
 
-------------------------------------------------------------------------------
--- Ex 9: given a tree, a path and a value, set the value at the end of
--- the path to the given value. Since Haskell datastructures are
--- immutable, you'll need to build a new tree.
---
--- If the path falls off the tree, do nothing.
---
--- Examples:
---   set [] 1 (Node 0 Empty Empty)  ==>  (Node 1 Empty Empty)
---   set [StepL,StepL] 1 (Node 0 (Node 0 (Node 0 Empty Empty)
---                                       (Node 0 Empty Empty))
---                               (Node 0 Empty Empty))
---                  ==>  (Node 0 (Node 0 (Node 1 Empty Empty)
---                                       (Node 0 Empty Empty))
---                               (Node 0 Empty Empty))
---
---   set [StepL,StepR] 1 (Node 0 Empty Empty)  ==>  (Node 0 Empty Empty)
-
+-- Ex 9
 set :: [Step] -> a -> Tree a -> Tree a
-set path val tree = todo
+set [] val Empty = Empty
+set [] val (Node _ left right) = Node val left right
+set (StepL:steps) val Empty = Empty
+set (StepL:steps) val (Node v left right) = Node v (set steps val left) right
+set (StepR:steps) val Empty = Empty
+set (StepR:steps) val (Node v left right) = Node v left (set steps val right)
 
-------------------------------------------------------------------------------
--- Ex 10: given a value and a tree, return a path that goes from the
--- root to the value. If the value doesn't exist in the tree, return Nothing.
---
--- You may assume the value occurs in the tree at most once.
---
--- Examples:
---   search 1 (Node 2 (Node 1 Empty Empty) (Node 3 Empty Empty))  ==>  Just [StepL]
---   search 1 (Node 2 (Node 4 Empty Empty) (Node 3 Empty Empty))  ==>  Nothing
---   search 1 (Node 2 (Node 3 (Node 4 Empty Empty)
---                            (Node 1 Empty Empty))
---                    (Node 5 Empty Empty))                     ==>  Just [StepL,StepR]
-
+-- Ex 10
 search :: Eq a => a -> Tree a -> Maybe [Step]
-search = todo
+search _ Empty = Nothing
+search val (Node v left right)
+  | val == v  = Just []
+  | otherwise = case search val left of
+      Just steps -> Just (StepL : steps)
+      Nothing -> case search val right of
+        Just steps -> Just (StepR : steps)
+        Nothing -> Nothing
