119a24c22795515fcc3d169708c24d717ada6791
deepankargarg5-maker <deepankar.garg5@gmail.com>
2025-08-27 16:08:29 +0530

    Update Set16b.hs

--- exercises/Set16b.hs
+++ exercises/Set16b.hs
@@ -1,96 +1,60 @@
+{-# OPTIONS_GHC -Wno-noncanonical-monad-instances #-}
 module Set16b where
-
 import Mooc.Todo
 import Examples.Phantom
-
 import Data.Char (toUpper)
 
-------------------------------------------------------------------------------
--- Ex 1: Define a constant pounds with type Money GBP and a value of
--- 3. The type Money is imported from Example.Phantom but you'll need
--- to introduce GBP yourself.
+-- Ex 1
+-- Define phantom type GBP
+data GBP
 
-pounds = todo
+pounds :: Money GBP
+pounds = Money 3
 
-------------------------------------------------------------------------------
--- Ex 2: Implement composition for Rates. Give composeRates a
--- restricted type so that the currencies are tracked correctly.
---
--- Examples:
---   composeRates (Rate 1.5) (Rate 1.25) ==> Rate 1.875
---   composeRates eurToUsd usdToChf :: Rate EUR CHF
---   composeRates eurToUsd (invert eurToUsd) :: Rate EUR EUR
---   composeRates eurToUsd eurToUsd :: type error!
---   composeRates eurToUsd :: Rate USD to -> Rate EUR to
+-- Ex 2
+-- Compose rates with phantom currency tracking
+composeRates ::
+  Rate a b -> Rate b c -> Rate a c
+composeRates (Rate r1) (Rate r2) = Rate (r1 * r2)
 
 -- For testing
 usdToChf :: Rate USD CHF
 usdToChf = Rate 1.11
 
-composeRates rate1 rate2 = todo
+-- Ex 3
+-- Phantom types for names
+data First
+data Last
+data Full
 
-------------------------------------------------------------------------------
--- Ex 3: Tracking first, last and full names with phantom types. The
--- goal is to have the types:
---  * Name First - for first names
---  * Name Last - for last names
---  * Name Full - for full names
---
--- In this exercise, you should define the phantom types First, Last
--- and Full, and the parameterised type Name. Then implement the
--- functions fromName, toFirst and toLast. Give the functions the
--- commented-out types
---
--- Examples:
---  fromName (toFirst "bob") ==> "bob"
---  fromName (toLast "smith") ==> "smith"
---  toFirst "bob" :: Name First
---  toLast "smith" :: Name Last
+newtype Name a = Name String
 
+fromName :: Name a -> String
+fromName (Name s) = s
 
--- Get the String contained in a name
---fromName :: Name a -> String
-fromName = todo
+toFirst :: String -> Name First
+toFirst = Name
 
--- Build a Name First
---toFirst :: String -> Name First
-toFirst = todo
+toLast :: String -> Name Last
+toLast = Name
 
--- Build a Name Last
---toLast :: String -> Name Last
-toLast = todo
+-- Ex 4
+capitalize :: Name a -> Name a
+capitalize (Name "") = Name ""
+capitalize (Name (c:cs)) = Name (toUpper c : cs)
 
-------------------------------------------------------------------------------
--- Ex 4: Implement the functions capitalize and toFull.
---
--- toFull should combine a first and a last name into a full name. Give
--- toFull the correct type (see examples below).
---
--- capitalize should capitalize the first letter of a name. Give
--- capitalize the correct type (see examples below).
---
--- Examples:
---  toFull (toFirst "bob") (toLast "smith") :: Name Full
---  fromName (toFull (toFirst "bob") (toLast "smith"))
---    ==> "bob smith"
---  capitalize (toFirst "bob") :: Name First
---  fromName (capitalize (toFirst "bob")) ==> "Bob"
---  capitalize (toLast "smith") :: Name Last
---  fromName (capitalize (toLast "smith")) ==> "Smith"
+toFull :: Name First -> Name Last -> Name Full
+toFull (Name f) (Name l) = Name (f ++ " " ++ l)
 
-capitalize = todo
-
-toFull = todo
-
-------------------------------------------------------------------------------
--- Ex 5: Type classes can let you write code that handles different
--- phantom types differently. Define instances for the Render type
--- class such that:
---
---  render (Money 1.0 :: Money EUR) ==> "1.0e"
---  render (Money 1.0 :: Money USD) ==> "$1.0"
---  render (Money 1.0 :: Money CHF) ==> "1.0chf"
-
+-- Ex 5
 class Render currency where
   render :: Money currency -> String
 
+instance Render EUR where
+  render (Money amount) = show amount ++ "e"
+
+instance Render USD where
+  render (Money amount) = "$" ++ show amount
+
+instance Render CHF where
+  render (Money amount) = show amount ++ "chf"
